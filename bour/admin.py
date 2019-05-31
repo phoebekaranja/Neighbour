fdf486f61656db23cbd938cd43634ee7271c7dcf
phoebekaranja <phoebemurish@gmail.com>
2019-05-31 15:58:19 +0300

    load our admin page and see these new objects that we can start editing our models from our admin website

--- bour/admin.py
+++ bour/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Neighborhood,Profile,Business,Post,Health
+
 
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
+admin.site.register(Health)
