dc3aa12aac6be8f4949d1ea476808b131c5676fc
phoebekaranja <phoebemurish@gmail.com>
2019-05-31 15:31:27 +0300

    created neighbourhood class on models file

--- bour/models.py
+++ bour/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length = 50)
+    location = models.CharField(max_length = 500)
+    count = models.PositiveIntegerField()
