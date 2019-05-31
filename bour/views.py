90794b7a6c94ce9c9f725b51354c7af36ae0ca19
phoebekaranja <phoebemurish@gmail.com>
2019-05-31 12:53:11 +0300

    created first view functions

--- bour/views.py
+++ bour/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    business = Business.get_all()
+    neighborhood = Neighborhood.get_all()
+    post = Post.get_all()
+    return render(request,'index.html',{'business':business,'neighborhood':neighborhood, 'post':post})
